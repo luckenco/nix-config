a6db11575a613fd7b1cc606e5b0de9cef75343af
Constantin Luckenbach <cluckenbach@protonmail.com>
2026-06-16 11:37:29 +0200

    feat(pi): manage agent package settings

---
+++ modules/common/pi.nix
@@ -0,0 +1,44 @@
+{ lib, pkgs, ... }:
+let
+  managedPiSettings = (pkgs.formats.json { }).generate "pi-agent-managed-settings.json" {
+    packages = [
+      {
+        source = "git:git@github.com:luckenco/pinnacle";
+      }
+      {
+        source = "npm:pi-web-access";
+      }
+      {
+        source = "npm:@tmustier/pi-raw-paste";
+      }
+      {
+        source = "npm:pi-btw";
+      }
+      {
+        source = "npm:pi-manage-todo-list";
+      }
+    ];
+  };
+in
+{
+  home-manager.sharedModules = [
+    (
+      { config, lib, ... }:
+      {
+        home.activation.writePiPackageSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          target="${config.home.homeDirectory}/.pi/agent/settings.json"
+          mkdir -p "$(dirname "$target")"
+
+          if [ -f "$target" ]; then
+            tmp="$(${pkgs.coreutils}/bin/mktemp)"
+            ${pkgs.jq}/bin/jq -s '.[0] * .[1]' "$target" "${managedPiSettings}" > "$tmp"
+            ${pkgs.coreutils}/bin/install -m 0644 "$tmp" "$target"
+            rm -f "$tmp"
+          else
+            ${pkgs.coreutils}/bin/install -m 0644 "${managedPiSettings}" "$target"
+          fi
+        '';
+      }
+    )
+  ];
+}
